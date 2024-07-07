d263cdf379235f58b498ff308845cdbe50accf3d
Евгений Кучма <desferto87@gmail.com>
2024-07-07 20:22:44 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textInString = "Дана строка с текстом, используя метод строки String.Split() " +
+                "получить массив слов, которые разделены пробелом в тексте и " +
+                "вывести массив, каждое слово с новой строки.";
+
+            char[] separator = new char[] { ',', ' ', '.' };
+
+            string[] subString = textInString.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in subString)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
